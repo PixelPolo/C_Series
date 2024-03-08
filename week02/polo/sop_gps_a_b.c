e5172f77c00cec16ba09a82af25101d00776e545
PixelPolo <paulr237@gmail.com>
2024-03-08 20:59:24 +0100

    GPS A and B done

---
+++ week02/polo/sop_gps_a_b.c
@@ -0,0 +1,38 @@
+/*
+Please do not change the name of this file.
+
+Name (of all group members): Paul Ricci & Julien Richoz
+Date: 03-08-2024
+Program runs (yes or no, because): yes
+*/
+
+#include <stdio.h>
+
+// A. Structure
+// your code here
+struct GpsPoint
+{
+    double latitude;
+    double longitude;
+    float altitude;
+};
+
+// B. Display a point
+// your code here
+void display(struct GpsPoint *point)
+{
+    printf("GPSPoint { Lat : %f | Lon : %f | Alt : %f\n",
+           point->altitude,
+           point->latitude,
+           point->altitude);
+}
+
+// ***** main *****
+int main(int argc, char *argv[])
+{
+    // Test display
+    struct GpsPoint point = {45.97639, 7.65833, 4478};
+    display(&point);
+
+    return 0;
+}
--- week02/polo/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-    printf("%c\n", answer[17]);
-    printf("%c\n", answer[34]);
-    printf("%c\n", answer[51]);
-    printf("%c\n", answer[13]);
-    printf("%c\n", answer[31]);
-    printf("%c\n", answer[49]);
